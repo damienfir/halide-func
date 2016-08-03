83579bccc4b11fb7a302b635a3cd985f3d2bcb6d
Damien Firmenich <fir.damien@gmail.com>
2016-08-03 11:59:30 +0200

    fixed bilfilter

--- facesmoothing.cpp
+++ facesmoothing.cpp
@@ -12,19 +12,20 @@ using namespace Halide::Tools;
 #define PI 3.14159
 
 
-Func bilateralFilter(Func im, const float sigmaDomain, const float sigmaRange) {
+Func bilateralFilter(Func im, float sigmaDomain, float sigmaRange) {
   Var x, y, i, j, v;
-  RDom d(cast<int>(-3*sigmaRange), cast<int>(6*sigmaRange+1),
-         cast<int>(-3*sigmaRange), cast<int>(6*sigmaRange+1));
-  Func fdomain, frange, w, out;
+  RDom d(-2*sigmaRange, 4*sigmaRange+1,
+         -2*sigmaRange, 4*sigmaRange+1);
+  Func w, out("out"), diff;
 
-  fdomain(v) = exp(-(v*v)/2.f*sigmaDomain*sigmaDomain);
-  frange(x, y) = exp(-(x*x+y*y)/2.f*sigmaRange*sigmaRange);
+  diff(x, y, i, j) = im(x,y) - im(x+i, y+j);
 
-  // w(x, y, i, j) = fdomain(im(x,y) - im(x+i, y+j)) * frange(i, j);
-  w(x, y, i, j) = 1.f;
+  w(x, y, i, j) = exp(-(diff(x,y,i,j)*diff(x,y,i,j))/(2.f*sigmaDomain*sigmaDomain)
+                      -(i*i+j*j)/(2.f*sigmaRange*sigmaRange));
 
   out(x, y) = sum(im(x+d.x, y+d.y) * w(x, y, d.x, d.y)) / sum(w(x, y, d.x, d.y));
+
+  w.compute_root();
 
   return out;
 }
@@ -46,17 +47,22 @@ int main(int argc, char **argv) {
   nir_scaled(x, y) = nir_clamped(x, y) / 255.f;
 
   Func vis_base, nir_base, nir_details;
-  vis_base = bilateralFilter(vis_gray, 10, 1);
-  nir_base = bilateralFilter(nir_scaled, 1, 1);
+  vis_base = bilateralFilter(vis_gray, 0.1f, 1);
+  nir_base = bilateralFilter(nir_scaled, 0.1f, 1);
   nir_details(x, y) = nir_scaled(x,y) - nir_base(x,y);
 
   Func smoothed_gray, smoothed, smoothed_rgb;
-  smoothed_gray(x, y) = 100.f * (vis_base(x, y) + nir_details(x, y));
-  smoothed(x, y, c) = select(c == 0, 100.f * vis_base(x,y), vis_lab(x,y,c));
+  smoothed_gray(x, y) = 100 * (vis_base(x, y) + nir_details(x, y));
+  smoothed(x, y, c) = select(c == 0, smoothed_gray(x,y), vis_lab(x,y,c));
   smoothed_rgb = labToRgb(smoothed);
+  // smoothed_rgb(x, y, c) = 255 * vis_base(x,y);
 
   Func result;
   result(x, y, c) = cast<uint8_t>(smoothed_rgb(x,y,c));
+
+  // Schedule
+  vis_gray.compute_root();
+  vis_base.compute_root();
 
   Image<uint8_t> output = result.realize(vis.width(), vis.height(), vis.channels());
   save_image(output, "output.png");
